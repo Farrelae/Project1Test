c5691eed1c500e38aa26f692c4a4722a5d729763
Andrew Farrell <afarrell849@gmail.com>
2021-03-04 14:38:21 -0500

    Website Body Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // API KEY: o+1CxAny3jhZGhUz9jk47g==RS6EhWjfHAcr5VGt
+
+// ------------------ Hamburger Menu -----------------------
+const menuController = document.getElementById('menu-hamburger');
+const main = document.querySelector('main');
+// define the click handler for the hamburger icon
+function handleMenuControllerClick() {
+	const menu = document.querySelector('.menu');
+	menu.classList.add('menu-open');
+	main.style.marginLeft = '250px';
+}
+
+// add an event listener to the hamburger icon
+menuController.addEventListener('click', handleMenuControllerClick);
+
+const menuClose = document.getElementById('menu-close');
+
+function handleMenuCloseClick() {
+	const menu = document.querySelector('.menu');
+	menu.classList.remove('menu-open');
+	main.style.marginLeft = '0px';
+}
+
+menuClose.addEventListener('click', handleMenuCloseClick);
